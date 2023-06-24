b1cf6fcec1603f1822d543d3539037d543e9c612
kino-ma <ma@kino.ma>
2023-06-24 13:41:47 +0900

    feat: verify jwt

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,7 @@
-export type TokenContent = {
+import jwt from "jsonwebtoken";
+import { env } from "./env";
+
+export type Payload = {
   userID: string;
 };
 
@@ -10,4 +13,18 @@ export const parseBearer = (authorizaton: string): string | null => {
 
   const token = authorizaton.substring(bearerText.length, authorizaton.length);
   return token;
+};
+
+export const verifyToken = (jwtString: string): Promise<Payload> => {
+  const promise = new Promise<Payload>((resolve, reject) => {
+    jwt.verify(jwtString, env.secret, {}, (err, decoded) => {
+      if (err !== null) {
+        reject(err);
+      } else {
+        resolve(decoded as Payload);
+      }
+    });
+  });
+
+  return promise;
 };
