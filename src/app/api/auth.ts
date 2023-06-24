5f9bdb091e2fc776e82c5d4bb842cf038887b01c
kino-ma <ma@kino.ma>
2023-06-24 14:27:20 +0900

    fix: authentication api

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -1,18 +1,24 @@
+import { COOKIE_NAME_TOKEN, signToken } from "@/lib/auth";
+import { setCookie } from "@/lib/cookie";
+import { statusInternalServerError, statusUnauthorized } from "@/lib/http";
 import { NextApiHandler } from "next";
 
 const auth: NextApiHandler = async (req, res) => {
   try {
     const { userID, password } = req.body;
     const authed = await lib.ldap.auth(userID, password);
+
     if (authed) {
-      const token = jwt.sign({ userID }, config.secret);
-      res.json({ ok: true, token });
+      const token = await signToken(userID);
+      setCookie(res, COOKIE_NAME_TOKEN, token);
+
+      res.json({ ok: true });
     } else {
       const err = new Error("invalid userID or password");
-      res.status(401).json(err);
+      res.status(statusUnauthorized).json(err);
     }
   } catch (err) {
-    res.status(500).json(err);
+    res.status(statusInternalServerError).json(err);
   }
 };
 
