9cf55ecd66254fe7cd88f742570b0d69be281589
kino-ma <ma@kino.ma>
2023-06-25 13:43:02 +0900

    fix: /api/auth as route handler

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -2,24 +2,25 @@ import { COOKIE_NAME_TOKEN, signToken } from "@/lib/auth";
 import { setCookie } from "@/lib/cookie";
 import { statusInternalServerError, statusUnauthorized } from "@/lib/http";
 import { NextApiHandler } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-const auth: NextApiHandler = async (req, res) => {
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
   try {
-    const { userID, password } = req.body;
+    const { userID, password } = await req.json();
     const authed = await lib.ldap.auth(userID, password);
 
     if (authed) {
       const token = await signToken(userID);
+      const res = NextResponse.json({ ok: true });
       setCookie(res, COOKIE_NAME_TOKEN, token);
-
-      res.json({ ok: true });
+      return res;
     } else {
       const err = new Error("invalid userID or password");
-      res.status(statusUnauthorized).json(err);
+      const res = NextResponse.json(err, { status: statusUnauthorized });
+      return res;
     }
   } catch (err) {
-    res.status(statusInternalServerError).json(err);
+    const res = NextResponse.json(err, { status: statusInternalServerError });
+    return res;
   }
 };
-
-export default auth;
