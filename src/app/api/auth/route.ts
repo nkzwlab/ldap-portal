fd1a28f985402e933d5b7a1e3b2227b21c65e12f
kino-ma <ma@kino.ma>
2023-07-01 16:02:30 +0900

    feat: auth with new ldap lib

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,15 +1,16 @@
 import { COOKIE_NAME_TOKEN, signToken } from "@/lib/auth";
 import { setCookie } from "@/lib/cookie";
 import { statusInternalServerError, statusUnauthorized } from "@/lib/http";
+import { auth } from "@/lib/ldap";
 import { NextApiHandler } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest): Promise<NextResponse> => {
   try {
     const { userID, password } = await req.json();
-    const authed = await lib.ldap.auth(userID, password);
+    const authSuccess = await auth(userID, password);
 
-    if (authed) {
+    if (authSuccess) {
       const token = await signToken(userID);
       const res = NextResponse.json({ ok: true });
       setCookie(res, COOKIE_NAME_TOKEN, token);
@@ -20,6 +21,7 @@ export const POST = async (req: NextRequest): Promise<NextResponse> => {
       return res;
     }
   } catch (err) {
+    console.error("POST /api/auth: An error occured:", err);
     const res = NextResponse.json(err, { status: statusInternalServerError });
     return res;
   }
