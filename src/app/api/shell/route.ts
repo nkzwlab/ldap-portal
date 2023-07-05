49ca23a8076cd625d83c655bc71e038e51d6eda6
kino-ma <ma@kino.ma>
2023-07-05 13:38:16 +0900

    feat: update shell to Next Router

---
+++ src/app/api/shell/route.ts
@@ -0,0 +1,34 @@
+import { HEADER_USERID } from "@/lib/auth";
+import { statusBadRequest, statusUnauthorized } from "@/lib/http";
+import * as ldap from "@/lib/ldap";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest): Promise<NextResponse> => {
+  const userID = req.headers.get(HEADER_USERID);
+  if (userID === null) {
+    console.error("GET /api/shell: userID not found in header");
+    return new NextResponse(null, { status: statusUnauthorized });
+  }
+
+  const shell = await ldap.getShell(userID);
+  return NextResponse.json({ ok: true, shell });
+};
+
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  const userID = req.headers.get(HEADER_USERID);
+  if (userID === null) {
+    console.error("GET /api/shell: userID not found in header");
+    return new NextResponse(null, { status: statusUnauthorized });
+  }
+
+  const { shell } = await req.json();
+  if (typeof shell === "string" || shell === "") {
+    return NextResponse.json(
+      { error: "invalid shell: " + shell },
+      { status: statusBadRequest }
+    );
+  }
+
+  await ldap.setShell(userID, shell);
+  return NextResponse.json({ ok: true, shell });
+};
