95480dd6255c85c9179184455e6b7a7cd8e707b1
kino-ma <ma@kino.ma>
2023-07-06 00:05:07 +0900

    refactor: change to functional component

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
@@ -11,112 +11,25 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 import styles from "./page.module.css";
 
-class Password extends React.Component {
-  constructor(props) {
-    super(props);
-    const token = window.localStorage.getItem("token");
-    this.state = {
-      err: null,
-      progress: false,
-      token,
-      password: "",
-      newPassword: "",
-      confirmNewPassword: "",
-    };
-  }
-  render() {
-    return (
-      <div className={this.props.classes.root}>
-        <Snackbar
-          open={this.state.err != null}
-          autoHideDuration={2000}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-          }}
-          onClose={() => {
-            this.setState({ err: null });
-          }}
-        >
-          <SnackbarContent message={this.state.err} />
-        </Snackbar>
-        <Grid container justify="center">
-          <Paper className={this.props.classes.paper}>
-            <form onSubmit={this.changePassword.bind(this)}>
-              <Grid container spacing={24}>
-                <Grid item xs={12}>
-                  <Typography gutterBottom variant="h5" align="center">
-                    Change Password
-                  </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Current Password"
-                    className={this.props.classes.password}
-                    type="password"
-                    autoComplete="current-password"
-                    onChange={(e) => {
-                      this.setState({ password: e.target.value });
-                    }}
-                    value={this.state.password}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="New Password"
-                    className={this.props.classes.password}
-                    type="password"
-                    autoComplete="new-password"
-                    onChange={(e) => {
-                      this.setState({ newPassword: e.target.value });
-                    }}
-                    error={
-                      this.state.newPassword.length < 8 &&
-                      this.state.newPassword.length != 0
-                    }
-                    value={this.state.newPassword}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Confirm New Password"
-                    className={this.props.classes.password}
-                    type="password"
-                    autoComplete="new-password"
-                    onChange={(e) => {
-                      this.setState({ confirmNewPassword: e.target.value });
-                    }}
-                    error={
-                      this.state.newPassword != this.state.confirmNewPassword
-                    }
-                    value={this.state.confirmNewPassword}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  {this.state.progress ? (
-                    <Grid container justify="center">
-                      <CircularProgress />
-                    </Grid>
-                  ) : (
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      color="primary"
-                      className={this.props.classes.button}
-                    >
-                      Submit
-                    </Button>
-                  )}
-                </Grid>
-              </Grid>
-            </form>
-          </Paper>
-        </Grid>
-      </div>
-    );
-  }
+interface State {
+  err: any;
+  progress: boolean;
+  password: string;
+  newPassword: string;
+  confirmNewPassword: string;
+}
 
-  async changePassword(e) {
+export default function Password() {
+  const [state, setState] = useState({
+    err: null,
+    progress: false,
+    token,
+    password: "",
+    newPassword: "",
+    confirmNewPassword: "",
+  });
+
+  async function changePassword(e) {
     e.preventDefault();
     const { token, password, newPassword } = this.state;
     if (
@@ -144,6 +57,94 @@ class Password extends React.Component {
       });
     }
   }
-}
 
-export default container(styles)(Password);
+  return (
+    <div className={this.props.classes.root}>
+      <Snackbar
+        open={this.state.err != null}
+        autoHideDuration={2000}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        onClose={() => {
+          this.setState({ err: null });
+        }}
+      >
+        <SnackbarContent message={this.state.err} />
+      </Snackbar>
+      <Grid container justify="center">
+        <Paper className={this.props.classes.paper}>
+          <form onSubmit={this.changePassword.bind(this)}>
+            <Grid container spacing={24}>
+              <Grid item xs={12}>
+                <Typography gutterBottom variant="h5" align="center">
+                  Change Password
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Current Password"
+                  className={this.props.classes.password}
+                  type="password"
+                  autoComplete="current-password"
+                  onChange={(e) => {
+                    this.setState({ password: e.target.value });
+                  }}
+                  value={this.state.password}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="New Password"
+                  className={this.props.classes.password}
+                  type="password"
+                  autoComplete="new-password"
+                  onChange={(e) => {
+                    this.setState({ newPassword: e.target.value });
+                  }}
+                  error={
+                    this.state.newPassword.length < 8 &&
+                    this.state.newPassword.length != 0
+                  }
+                  value={this.state.newPassword}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Confirm New Password"
+                  className={this.props.classes.password}
+                  type="password"
+                  autoComplete="new-password"
+                  onChange={(e) => {
+                    this.setState({ confirmNewPassword: e.target.value });
+                  }}
+                  error={
+                    this.state.newPassword != this.state.confirmNewPassword
+                  }
+                  value={this.state.confirmNewPassword}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                {this.state.progress ? (
+                  <Grid container justify="center">
+                    <CircularProgress />
+                  </Grid>
+                ) : (
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    className={this.props.classes.button}
+                  >
+                    Submit
+                  </Button>
+                )}
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </Grid>
+    </div>
+  );
+}
