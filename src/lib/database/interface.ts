7abd1d02cc97225388035b97c2b61af0b0881f2a
kino-ma <ma@kino.ma>
2023-07-15 03:07:59 +0900

    feat: convert unknown data to Application

--- src/lib/database/interface.ts
+++ src/lib/database/interface.ts
@@ -10,3 +10,22 @@ export type Application = {
   email: string;
   token: string;
 };
+
+export const applicationFromJson = (data: unknown): Application | null => {
+  const keys: (keyof Application)[] = ["loginName", "email", "token"];
+  const out: Application = {
+    loginName: "",
+    email: "",
+    token: "",
+  };
+
+  for (const key of keys) {
+    if (typeof (data as any)[key] === "undefined") {
+      return null;
+    }
+
+    out[key] = (data as any)[key];
+  }
+
+  return out;
+};
