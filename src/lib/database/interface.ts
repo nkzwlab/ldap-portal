c1ad87a37e14e870d9254a08f2db6636592dc616
kino-ma <ma@kino.ma>
2023-07-15 02:58:54 +0900

    feat: allow asynchronous operations

--- src/lib/database/interface.ts
+++ src/lib/database/interface.ts
@@ -1,8 +1,8 @@
 export interface ApplicationRepository {
-  addApplication(application: Application): void;
-  getApplication(loginName: string): Application | null;
-  getApplicationByToken(token: string): Application | null;
-  deleteApplication(loginName: string): void;
+  addApplication(application: Application): Promise<void>;
+  getApplication(loginName: string): Promise<Application | null>;
+  getApplicationByToken(token: string): Promise<Application | null>;
+  deleteApplication(loginName: string): Promise<void>;
 }
 
 export type Application = {
