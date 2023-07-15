c301975d0c3ddc3d5d8f3cc8d4dca808e06b8bf8
kino-ma <ma@kino.ma>
2023-07-15 12:08:28 +0900

    feat: hold global redis repository instance

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,1 +1,16 @@
+import { ApplicationRepository } from "./interface";
+import { RedisRepository } from "./redis";
+
 export * from "./interface";
+export * from "./memory";
+export * from "./redis";
+
+let redisRepository: ApplicationRepository | null = null;
+
+export const getRedisRepository = async (): Promise<ApplicationRepository> => {
+  if (redisRepository === null) {
+    redisRepository = await RedisRepository.withDefaultConfiguration();
+  }
+
+  return redisRepository;
+};
