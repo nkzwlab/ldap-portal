9641ec993a7758d3307dd8b57c51fd021a775598
kino-ma <ma@kino.ma>
2023-07-15 12:14:29 +0900

    feat: POST /register

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,34 @@
+import { SSHA, generateToken } from "@/lib/crypto";
+import { Application, getRepository } from "@/lib/database";
+import { statusBadRequest, statusOk, statusUnauthorized } from "@/lib/http";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  const { loginName, email, password } = await req.json();
+
+  if (
+    typeof loginName !== "string" ||
+    typeof email !== "string" ||
+    typeof password !== "string"
+  ) {
+    return NextResponse.json(
+      { error: "invalid loginName or email or password" },
+      { status: statusBadRequest }
+    );
+  }
+
+  const token = generateToken();
+  const ssha = await SSHA.withRandomSalt(password);
+  const passwordHash = ssha.passwd;
+  const application: Application = {
+    loginName,
+    email,
+    passwordHash,
+    token,
+  };
+
+  const repository = await getRepository();
+  await repository.addApplication(application);
+
+  return NextResponse.json({ success: true }, { status: statusOk });
+};
