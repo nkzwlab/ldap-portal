96a7986fe4335dea7962ebbdfa0bf0b35090d1cc
kino-ma <ma@kino.ma>
2023-07-15 12:11:13 +0900

    feat: use in-memory repository on tests

--- src/lib/database/index.ts
+++ src/lib/database/index.ts
@@ -1,13 +1,20 @@
+import { env } from "../env";
 import { ApplicationRepository } from "./interface";
+import { InMemmoryRepository } from "./memory";
 import { RedisRepository } from "./redis";
 
 export * from "./interface";
 export * from "./memory";
 export * from "./redis";
 
+let memoryRepository = new InMemmoryRepository();
 let redisRepository: ApplicationRepository | null = null;
 
-export const getRedisRepository = async (): Promise<ApplicationRepository> => {
+export const getRepository = async (): Promise<ApplicationRepository> => {
+  if (env.isTest) {
+    return memoryRepository;
+  }
+
   if (redisRepository === null) {
     redisRepository = await RedisRepository.withDefaultConfiguration();
   }
