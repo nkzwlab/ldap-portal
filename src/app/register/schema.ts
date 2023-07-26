47e9b76004d7c4b378ba5248cbe77dfc5b116197
kino-ma <ma@kino.ma>
2023-07-26 12:48:21 +0900

    feat: add regstration form schema

---
+++ src/app/register/schema.ts
@@ -0,0 +1,20 @@
+import { passwordSchema } from "@/lib/schemas";
+import * as z from "zod";
+export const schema = z
+  .object({
+    loginName: z
+      .string()
+      .min(1, { message: "Login name is required" })
+      .regex(/^[0-9A-Za-z_-]+$/, "Login name contains invalid character"),
+    password: passwordSchema,
+    passwordConfirmation: passwordSchema,
+  })
+  .superRefine(({ password, passwordConfirmation }, ctx) => {
+    if (password !== passwordConfirmation) {
+      ctx.addIssue({
+        path: ["passwordConfirmation"],
+        code: "custom",
+        message: "Password and password confirmation do not match",
+      });
+    }
+  });
