0eb1c23988fbe8501b4418720b451d728f005da0
kino-ma <ma@kino.ma>
2023-11-01 20:08:17 +0900

    refactor: include token in application data

--- src/lib/database/application/interface.ts
+++ src/lib/database/application/interface.ts
@@ -8,15 +8,22 @@ export type Application = {
   loginName: string;
   email: string;
   passwordHash: string;
+  token: string;
 };
 
 export const applicationFromJson = (data: unknown): Application | null => {
-  const keys: (keyof Application)[] = ["loginName", "email", "passwordHash"];
+  const keys: (keyof Application)[] = [
+    "loginName",
+    "email",
+    "passwordHash",
+    "token",
+  ];
   const optionalKeys: (typeof keys)[number][] = ["email"];
   const out: Application = {
     loginName: "",
     email: "",
     passwordHash: "",
+    token: "",
   };
 
   for (const key of keys) {
