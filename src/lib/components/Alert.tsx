3a8dfdddfd99f134b6146c52df1c2fc9f751ae8d
kino-ma <ma@kino.ma>
2023-11-12 08:36:16 +0900

    feat: add csutom alert component

--- src/lib/components/Alert.tsx
+++ src/lib/components/Alert.tsx
@@ -1,1 +1,37 @@
-export default function Alert()
+import { PropsWithChildren, useState } from "react";
+import {
+  Alert as MuiAlert,
+  AlertProps as MuiAlertProps,
+  Snackbar,
+} from "@mui/material";
+
+export interface AlertProps extends PropsWithChildren {
+  open: boolean;
+  handleClose: () => void;
+}
+
+export default function Alert({ open, handleClose, children }: AlertProps) {
+  const handleAlertClose = (
+    _event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    handleClose();
+  };
+
+  return (
+    <Snackbar open={open} autoHideDuration={6000} onClose={handleAlertClose}>
+      <MuiAlert
+        onClose={handleAlertClose}
+        severity="success"
+        variant="filled"
+        sx={{ width: "100%" }}
+      >
+        {children}
+      </MuiAlert>
+    </Snackbar>
+  );
+}
