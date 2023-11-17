472860661ca0f507c156600d83b45e9098394d79
kino-ma <ma@kino.ma>
2023-11-17 14:31:02 +0900

    feat: add logout api

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,21 @@
+import { COOKIE_NAME_TOKEN, COOKIE_NAME_USERID } from "@/lib/auth/consts";
+import { setCookie } from "@/lib/cookie";
+import {
+  statusInternalServerError,
+  statusUnauthorized,
+} from "@/lib/http/status";
+import { auth } from "@/lib/ldap";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export type ApiLoginParams = {
+  loginName: string;
+  password: string;
+};
+
+export const POST = async (req: NextRequest): Promise<NextResponse> => {
+  cookies().delete(COOKIE_NAME_TOKEN);
+  cookies().delete(COOKIE_NAME_USERID);
+
+  return NextResponse.json({ success: true });
+};
