75735da0d39dea23fbb83936ec4282b0623d8a9c
kino-ma <ma@kino.ma>
2023-11-18 14:43:58 +0900

    feat: redirect to login page after logging out

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -17,5 +17,8 @@ export const POST = async (req: NextRequest): Promise<NextResponse> => {
   cookies().delete(COOKIE_NAME_TOKEN);
   cookies().delete(COOKIE_NAME_USERID);
 
-  return NextResponse.json({ success: true });
+  const loginUrl = req.nextUrl.clone();
+  loginUrl.pathname = "/login";
+
+  return NextResponse.redirect(loginUrl);
 };
