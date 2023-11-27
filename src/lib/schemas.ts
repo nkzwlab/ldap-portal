af597be025883169d05b92a70902bbbe1866e0d4
kino-ma <ma@kino.ma>
2023-11-27 21:24:38 +0900

    feat: add symbols allowed in password

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -7,9 +7,12 @@ export const passwordSchema = z
 export const newPasswordSchema = z
   .string()
   .min(8, { message: "Password must be 8 charatcers long or more" })
-  .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&,._-])[A-Za-z\d@$!%*?&,._-]+$/, {
-    message: "Password must include at least 1 alphabet, number, and symbol",
-  })
+  .regex(
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&#/(),._-])[A-Za-z\d@$!%*?&#/(),._-]+$/,
+    {
+      message: "Password must include at least 1 alphabet, number, and symbol",
+    }
+  )
   .max(256, { message: "Password is too long" });
 
 export const confirmPassword: z.SuperRefinement<{
